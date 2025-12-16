da21e2227230ab8b23822ade290ebc32576422ff
root <wenji.yyc@alibaba-inc.com>
2025-12-16 02:32:37 +0000

    upd

--- klib/ops/add/add_kernel.cc
+++ klib/ops/add/add_kernel.cc
@@ -1,10 +1,12 @@
 #include <cuda.h>
 
+#define TVM_FFI_CUDA_USE_DRIVER_API 1
+
 #include <tvm/ffi/container/tensor.h>
+#include <tvm/ffi/extra/c_env_api.h>
 #include <tvm/ffi/extra/cuda/cubin_launcher.h>
 
-#include "driver_types.h"
-#include "tvm_ffi_utils.h"
+#include <tvm/ffi/extra/cuda/unify_api.h>
 
 #include "add_kernel.h"
 
@@ -32,12 +34,12 @@ void add_kernel(const tvm::ffi::TensorView& input) {
     int threadsPerBlock = 256;
     int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
 
-    CUstream stream = static_cast<CUstream>(TVMFFIEnvGetStream(input.device().device_type, input.device().device_id));
+    tvm::ffi::StreamHandle stream = static_cast<tvm::ffi::StreamHandle>(TVMFFIEnvGetStream(input.device().device_type, input.device().device_id));
 
     tvm::ffi::dim3 grid((N + threadsPerBlock - 1) / threadsPerBlock);
     tvm::ffi::dim3 block(256);
 
-    cudaError_t result = kernel.Launch(args, grid, block, stream);
+    tvm::ffi::ResultHandle result = kernel.Launch(args, grid, block, stream);
     TVM_FFI_CHECK_CUDA_ERROR(result);
 }
 
