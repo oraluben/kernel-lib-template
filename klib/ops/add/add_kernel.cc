97c14a717b0a9140964bd461b09afccf9889654a
Yichen Yan <oraluben@outlook.com>
2026-01-20 11:37:49 +0800

    sync with upstream embed cubin launcher

--- klib/ops/add/add_kernel.cc
+++ klib/ops/add/add_kernel.cc
@@ -1,48 +1,30 @@
 #include <cuda.h>
 
 #include <tvm/ffi/container/tensor.h>
+#include <tvm/ffi/extra/c_env_api.h>
+#include <tvm/ffi/extra/cuda/cubin_launcher.h>
 
 #include "tvm_ffi_utils.h"
 
 #include "add_kernel.h"
 
-void add_kernel(const tvm::ffi::TensorView& input) {
-    CUresult res;
+TVM_FFI_EMBED_CUBIN(env);
 
-    CUmodule module;
-    res = cuModuleLoadFatBinary(&module, imageBytes);
-    if (res != CUDA_SUCCESS) {
-        std::cerr << "Failed to load fatbin! Error code: " << res << std::endl;
-        return;
-    }
+void add_kernel(const tvm::ffi::TensorView &input) {
+  static auto kernel = TVM_FFI_EMBED_CUBIN_GET_KERNEL(env, "AddOneKernel");
 
-    CUfunction kernel;
-    res = cuModuleGetFunction(&kernel, module, "AddOneKernel");
-    if (res != CUDA_SUCCESS) {
-        std::cerr << "Failed to load kernel! Error code: " << res << std::endl;
-        return;
-    }
+  int64_t n = input.numel();
+  tvm::ffi::dim3 grid((n + 255) / 256);
+  tvm::ffi::dim3 block(256);
 
-    void* input_ptr = input.data_ptr();
-    void *args[] = { &input_ptr };
+  auto stream = static_cast<tvm::ffi::cuda_api::StreamHandle>(
+      TVM_FFI_GET_CUDA_STREAM(input));
 
-    long N = input.numel();
-
-    int threadsPerBlock = 256;
-    int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
-
-    CUstream stream = static_cast<CUstream>(TVMFFIEnvGetStream(input.device().device_type, input.device().device_id));
-
-    cuLaunchKernel(
-        kernel,
-        blocksPerGrid, 1, 1,
-        threadsPerBlock, 1, 1,
-        0,
-        stream,
-        args,
-        NULL
-    );
+  void *input_ptr = input.data_ptr();
+  void *args[] = {&input_ptr};
+  tvm::ffi::cuda_api::ResultType result =
+      kernel.Launch(args, grid, block, stream);
+  TVM_FFI_CHECK_CUBIN_LAUNCHER_CUDA_ERROR(result);
 }
-
 
 TVM_FFI_DLL_EXPORT_TYPED_FUNC(add, add_kernel);
